ba0c3fee5de4d6ecc56c9c4b65502869fec74bd8
Mostafa Hisham <mmham3919@gmail.com>
2021-02-21 17:57:57 +0200

    adding song model

---
+++ node-apis-service/models/song.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const SongSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    singerName: {
+        type: String,
+        required: true,
+    },
+    mood: {
+        type: String,
+        required: true,
+    },
+    album: {
+        type: String,
+        required: true,
+    },
+    ocasion: {
+        type: String,
+        required: true,
+    },
+    ocasion: {
+        type: String,
+        required: true,
+    },
+    year: {
+       type: String,
+       required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Song', SongSchema);

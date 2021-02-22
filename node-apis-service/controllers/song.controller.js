fae8bcce81a53bb5a38bb81aefc0eede18131a22
Mostafa Hisham <mmham3919@gmail.com>
2021-02-22 16:43:00 +0200

    fix createSong controller

--- node-apis-service/controllers/song.controller.js
+++ node-apis-service/controllers/song.controller.js
@@ -1,11 +1,21 @@
 const SongModel = require('./../models/song.model');
 const randomNumberGenerator =  require('./../helpers/randomNumberGenerator');
 const randomStringGenerator = require('./../helpers/randomStringGenerator');
+const axios = require('axios');
 // @desc Create Song
 // @route POST /api/v1/songs
 // @access Public
 const createSong = async (req, res) => {
     try {
+        const formData = new FormData();
+        formData.append('file', req.files);
+
+        const response = await axios.post(process.env.PYTHON_SERVICE_URI, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+         });
+
         // Create song
         const song = await SongModel.create({
             name: randomStringGenerator(6),
@@ -14,7 +24,7 @@ const createSong = async (req, res) => {
             album: randomStringGenerator(6),
             ocasion: randomStringGenerator(6),
             year: randomNumberGenerator(2, 6),
-            genre: req.body.genre,
+            genre: response.data.data,
         });
 
         res.status(200).json({ 
